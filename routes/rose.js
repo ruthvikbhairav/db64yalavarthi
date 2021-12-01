e01825a867c4bce4de1e9a0717cc83a73bafd367
ruthvikbhairav <89425392+ruthvikbhairav@users.noreply.github.com>
2021-11-30 19:20:27 -0600

    Delete rose.js

--- routes/rose.js
+++
@@ -1,7 +0,0 @@
-var express = require('express'); 
-const rose_controlers= require('../controllers/rose'); 
-var router = express.Router(); 
- 
-/* GET costumes */ 
-router.put('/rose/:id', rose_controlers.rose_update_put); 
-module.exports = router; 
